842131a5be3969dd6cfd7ce8894ad9d9356e1c7c
Philipp Schulz <hadeweka@outlook.de>
2023-05-07 13:54:56 +0200

    Removed some unnecessary code for now

--- src/graphics/Tileset.cr
+++ src/graphics/Tileset.cr
@@ -5,13 +5,6 @@
 module Crystal2Day
   class Tile
     @solid = false
-    @animation_frame = false
-  
-    # TODO: These currently don't do anything
-    @index_of_first_animation_frame : Crystal2Day::TileID = 0
-    @index_of_first_other_animation_frame : Crystal2Day::TileID = 0
-    @number_of_animation_frames : UInt32 = 0
-    @time_per_animation_frame : UInt32 = 0
   
     def initialize
     end
@@ -20,28 +13,8 @@ module Crystal2Day
       return @solid
     end
   
-    def is_animation_frame?
-      return @animation_frame
-    end
-  
     def solid=(value : Bool = true)
       @solid = value
-    end
-  
-    def get_animation_frame(frame_counter : UInt32)
-      animation_cycle = @number_of_animation_frames * @time_per_animation_frame
-      animation_time = frame_counter % animation_cycle
-      animation_index = animation_time // @time_per_animation_frame
-  
-      if animation_index == 0
-        return @index_of_first_animation_frame
-      else
-        return @index_of_first_other_animation_frame + animation_index - 1
-      end
-    end
-  
-    def set_animation(@index_of_first_animation_frame : Crystal2Day::TileID, @index_of_first_other_animation_frame : Crystal2Day::TileID, @number_of_animation_frames : UInt32, @time_per_animation_frame : UInt32)
-      @animation_frame = true
     end
   end
 
