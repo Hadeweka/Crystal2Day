b1fc4d7dd33c48b6d221fc871b7558f97a3622c5
Philipp Schulz <hadeweka@outlook.de>
2023-06-29 22:42:47 +0200

    Fixed collision example

--- example/ExampleCollisionTile.rb
+++ example/ExampleCollisionTile.rb
@@ -1,24 +1,22 @@
 each_frame do
   collision = entity.current_collision
 
+  # TODO: Add tile and entity size to this somehow
+
   if collision.tile.get_flag("solid")
-    if collision.other_position.x + 25 > entity.position.x
-      puts "Blocked: Right" if entity.velocity.x > 0
+    if collision.other_position.x + 25 > entity.aligned_position.x && (entity.aligned_position.y - collision.other_position.y - 25).abs < 50
       entity.velocity.x = 0 if entity.velocity.x > 0
     end
 
-    if collision.other_position.x + 25 < entity.position.x
-      puts "Blocked: Left" if entity.velocity.x < 0
+    if collision.other_position.x + 25 < entity.aligned_position.x && (entity.aligned_position.y - collision.other_position.y - 25).abs < 50
       entity.velocity.x = 0 if entity.velocity.x < 0
     end
 
-    if collision.other_position.y + 25 > entity.position.y
-      puts "Blocked: Down" if entity.velocity.y > 0
+    if collision.other_position.y + 25 > entity.aligned_position.y && (entity.aligned_position.x - collision.other_position.x - 25).abs < 50
       entity.velocity.y = 0 if entity.velocity.y > 0
     end
     
-    if collision.other_position.y + 25 < entity.position.y
-      puts "Blocked: Up" if entity.velocity.y < 0
+    if collision.other_position.y + 25 < entity.aligned_position.y && (entity.aligned_position.x - collision.other_position.x - 25).abs < 50
       entity.velocity.y = 0 if entity.velocity.y < 0
     end
   end
