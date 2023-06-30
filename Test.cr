8531f28e53fa0f1dfe624a9cee397ec5f75d82c8
Philipp Schulz <hadeweka@outlook.de>
2023-06-30 20:35:24 +0200

    Improved example

--- Test.cr
+++ Test.cr
@@ -23,6 +23,30 @@ CD.db.add_entity_proc("PlaySound") do |entity|
   sound.play
 end
 
+CD.db.add_entity_proc("TileCollision") do |entity|
+  entity.each_tile_collision do |collision|
+    # TODO: Add tile and entity size to this somehow
+
+    if collision.tile.get_flag("solid")
+      if collision.other_position.x + 25 > entity.aligned_position.x && (entity.aligned_position.y - collision.other_position.y - 25).abs < 50
+        entity.velocity.x = 0 if entity.velocity.x > 0
+      end
+
+      if collision.other_position.x + 25 < entity.aligned_position.x && (entity.aligned_position.y - collision.other_position.y - 25).abs < 50
+        entity.velocity.x = 0 if entity.velocity.x < 0
+      end
+
+      if collision.other_position.y + 25 > entity.aligned_position.y && (entity.aligned_position.x - collision.other_position.x - 25).abs < 50
+        entity.velocity.y = 0 if entity.velocity.y > 0
+      end
+      
+      if collision.other_position.y + 25 < entity.aligned_position.y && (entity.aligned_position.x - collision.other_position.x - 25).abs < 50
+        entity.velocity.y = 0 if entity.velocity.y < 0
+      end
+    end
+  end
+end
+
 WIDTH = 1600
 HEIGHT = 900
 
